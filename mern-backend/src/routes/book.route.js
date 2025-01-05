fe412d4d1bcb5af4b66adb9a39df78de654f7124
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-06 00:07:49 +0600

    Book creation or upload success

--- mern-backend/src/routes/book.route.js
+++ mern-backend/src/routes/book.route.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const bookModel = require("../model/bookModel");
 
+// Create
+router.post("/upload-book", async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await bookModel.create(data);
+    res.status(201).json({ status: "Success", data: result });
+  } catch (error) {
+    console.log("Failed to upload book!", error);
+    res.status(500).json({ status: "Error", message: "Failed to upload book" });
+  }
+});
+
+module.exports = router;
