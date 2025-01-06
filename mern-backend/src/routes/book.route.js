fb07e469093be2ecb0c3e375317cd63dead80bac
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-06 17:51:32 +0600

    Find by category and Get all book

--- mern-backend/src/routes/book.route.js
+++ mern-backend/src/routes/book.route.js
@@ -14,17 +14,30 @@ router.post("/upload-books", async (req, res) => {
   }
 });
 
-// Get all book
+// Find by category and Get all book
 router.get("/all-books", async (req, res) => {
     try {
-      const result = await bookModel.find()
-      res.status(201).json({status: "Success", data: result});
+        const category = req.query.category;
 
-    }catch (e) {
-      console.log("Failed to getting book!", e);
-      res.status(500).json({status: "Error", message: "Failed to getting books"});
+        let result;
+
+        if (category) {
+            result = await bookModel.find({ category: category });
+        } else {
+            result = await bookModel.find();
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ status: "Error", message: "No books found in this category" });
+        }
+
+        res.status(200).json({ status: "Success", data: result });
+    } catch (e) {
+        console.log("Failed to get books!", e);
+        res.status(500).json({ status: "Error", message: "Failed to get books" });
     }
-})
+});
+
 
 // Update a book
 router.patch('/update-books/:id', async (req, res) => {
