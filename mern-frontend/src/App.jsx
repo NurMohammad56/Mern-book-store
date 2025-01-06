0ee941b699560cdea826481a6c52b2406467a33c
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-06 21:35:54 +0600

    Frontend file and folder ready

--- mern-frontend/src/App.jsx
+++ mern-frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center h-screen text-3xl text-red-950 font-bold'>Hello</div>
+      <div className='flex items-center justify-center h-screen text-3xl text-red-950 font-bold'>Hello React</div>
     </>
   )
 }
