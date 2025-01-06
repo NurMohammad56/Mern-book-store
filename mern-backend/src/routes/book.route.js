c442cae8ffb0afbde26c8e6982dab3505987d987
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-06 13:56:35 +0600

    Getting all book completed

--- mern-backend/src/routes/book.route.js
+++ mern-backend/src/routes/book.route.js
@@ -3,15 +3,27 @@ const router = express.Router();
 const bookModel = require("../model/bookModel");
 
 // Create
-router.post("/upload-book", async (req, res) => {
+router.post("/upload-books", async (req, res) => {
   try {
     const data = req.body;
     const result = await bookModel.create(data);
     res.status(201).json({ status: "Success", data: result });
-  } catch (error) {
-    console.log("Failed to upload book!", error);
+  } catch (e) {
+    console.log("Failed to upload book!", e);
     res.status(500).json({ status: "Error", message: "Failed to upload book" });
   }
 });
+
+// Read
+router.get("/all-books", async (req, res) => {
+    try {
+      const result = await bookModel.find()
+      res.status(201).json({status: "Success", data: result});
+
+    }catch (e) {
+      console.log("Failed to getting book!", e);
+      res.status(500).json({status: "Error", message: "Failed to getting books"});
+    }
+})
 
 module.exports = router;
