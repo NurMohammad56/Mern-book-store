656290b6747b704a5b089e5aed8bd4b2b16a5333
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-06 16:52:24 +0600

    Delete a book api completed

--- mern-backend/src/routes/book.route.js
+++ mern-backend/src/routes/book.route.js
@@ -39,4 +39,16 @@ router.patch('/update-books/:id', async (req, res) => {
     }
 })
 
+// Delete a book
+router.delete('/delete-books/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const result = await bookModel.findByIdAndDelete(id)
+        res.status(201).json({status: "Success", data: result});
+    }catch (e) {
+        console.log("Failed to delete book!", e);
+        res.status(500).json({status: "Error", message: "Failed to delete book!", e});
+    }
+})
+
 module.exports = router;
