df1abe22c6acb097c2e75d0558852ec050e1d11f
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-12 23:53:40 +0600

    Nav item is done

--- mern-frontend/src/component/Navbar.jsx
+++ mern-frontend/src/component/Navbar.jsx
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import { FaBook } from "react-icons/fa";
+import {FaBarsStaggered} from "react-icons/fa6";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -23,8 +26,35 @@ const Navbar = () => {
             window.addEventListener("scroll", handleScroll)
         }
     },[])
+
+    // Nav items
+    const navItems = [
+        {link: "Home", path:"/"},
+        {link: "About", path:"/about"},
+        {link: "Shop", path:"/shop"},
+        {link: "Sell Your Book", path:"/admin/dashboard"},
+        {link: "Blog", path:"/blog"},
+    ]
     return (
-        <div>Navbar</div>
+        <header>
+            <nav>
+                <div>
+                {/*Logo*/}
+                    <Link to='/' className="text-2xl font-bold text-textColor flex items-center gap-2"><FaBook className="inline-block"/>Books</Link>
+
+                {/*Nav items for large device*/}
+                    <ul className="md:flex space-x-12">
+                        {
+                            navItems.map(({link, path}) => <Link to={path} key={path} className="uppercase cursor-pointer hover:text-textColor block text-base text-black">
+                                {link}
+                            </Link>)
+                        }
+                    </ul>
+
+
+                </div>
+            </nav>
+        </header>
     );
 };
 
