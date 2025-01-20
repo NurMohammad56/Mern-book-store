7aaf6cdc4207de5f7e44cc63f269ac3e0ab10a52
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-20 16:57:26 +0600

    left side design ready for banner

--- mern-frontend/src/component/Bannner.jsx
+++ mern-frontend/src/component/Bannner.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Slider from "./Slider.jsx";
+
+
 
 const Banner = () => {
     return (
-        <div className="px-4 lg:px-24 bg-gradient-to-r from-amber-200 to-purple-300  items-center">
+        <div className="px-4 lg:px-24 bg-green-200 items-center">
             <div className='flex flex-col md:flex-row justify-between w-full items-center gap-12 py-32'>
             {/*Left side*/}
                 <div className={`md:w-1/2 space-y-7 h-full`}>
-                <div className="text-5xl font-bold leading-snug text-black">Buy And Sell Your Book</div>
+                <h2 className="text-5xl font-bold leading-snug text-black">Buy And Sell Your Book <span className={`text-textColor`}>For the Best Prices</span></h2>
                 <p className={`md:w-4/5`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 
                 <div>
@@ -16,7 +19,9 @@ const Banner = () => {
                 </div>
 
             {/*Right side*/}
-                <div>Right side</div>
+                <div>
+                    <Slider/>
+                </div>
             </div>
         </div>
     );
