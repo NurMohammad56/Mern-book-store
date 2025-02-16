47b4c9f4262ec4d910ad2c150530655851b7560b
NurMohammad56 <nurmohammad0605@gmail.com>
2025-02-16 19:47:37 +0600

    using useLoaderData and render some data in browser complete

--- mern-frontend/src/component/home/SingleBook.jsx
+++ mern-frontend/src/component/home/SingleBook.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const SingleBook = () => {
+    const book = useLoaderData();
+    // const book = useLoaderData() || { _id: "12345", title: "Test Book" };
+
+
+    if (!book) {
+        return <div>Loading.......</div>;
+    }
+
     return (
-        <div>SingleBook</div>
+        <div>
+            <h1>Single Book Details : {book.data._id}</h1>
+
+        </div>
     );
+
 };
 
 export default SingleBook;
--- mern-frontend/src/routers/router.jsx
+++ mern-frontend/src/routers/router.jsx
@@ -4,10 +4,11 @@ import {
 } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/home/Home.jsx";
-import * as path from "node:path";
 import Shop from "../pages/shop/Shop.jsx";
 import About from "../pages/about/About.jsx";
 import Blog from "../pages/blog/Blog.jsx";
+import SingleBook from "../component/home/SingleBook.jsx";
+
 
 const router = createBrowserRouter([
     {
@@ -28,8 +29,25 @@ const router = createBrowserRouter([
             {
                 path: "/blog",
                 element: <Blog/>,
+            },
+            {
+                path: "/books/:id",
+                element: <SingleBook/>,
+                loader: async ({ params }) => {
+                    try {
+                        const response = await fetch(`http://localhost:5000/api/v1/books/${params.id}`);
+                        if (!response.ok) {
+                            throw new Error("Book not found");
+                        }
+                        const data = await response.json();
+                        console.log("Fetched data:", data);
+                        return data;
+                    } catch (error) {
+                        console.error("Error loading book:", error);
+                        return null;
+                    }
+                }
             }
-
         ],
     },
 ]);
